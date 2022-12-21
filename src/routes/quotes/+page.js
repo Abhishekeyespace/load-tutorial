e67cfeed9c00e2de0c09048856aabd6bfddc61a6
abhishekeyespace <abhishek@eyespacelenses.com>
2022-12-21 16:10:07 +1030

    experimenting parallel loading

--- src/routes/quotes/+page.js
+++ src/routes/quotes/+page.js
@@ -1,10 +1,30 @@
 /**
  *  fetch fuction behaves identically to the native fetch web API
- **/
+ *  Here, initial fetch is making a server side call, but then it makes
+ *  a client side call to the same endpoint
+ */
 
 export const load = async ({ fetch }) => {
-  const res = await fetch('https://dummyjson.com/quotes?limit=10')
-  const quotesData = await res.json()
-  const quotes = quotesData.quotes
-  return { quotes }
+
+  const fetchQuotes = async () => {
+    const quotesRes = await fetch('https://dummyjson.com/quotes?limit=10')
+    const quotesData = await quotesRes.json()
+    return quotesData.quotes
+  }
+
+  const  fetchUsers = async () => {
+    const usersRes = await fetch('https://dummyjson.com/users?limit=10')
+    const usersData = await usersRes.json()
+    return  usersData.users
+   
+  }
+
+  return { 
+    quotes:fetchQuotes(),
+    users:fetchUsers()
+  }
+
+
+
+
 }
--- src/routes/quotes/+page.svelte
+++ src/routes/quotes/+page.svelte
@@ -1,6 +1,58 @@
 <script>
-    export let data;
-    console.log(data);
+  export let data;
+  const {quotes,users} = data;
+  
 </script>
 
-<h1> Quotes </h1>
+<h1>Quotes</h1>
+<div>
+{#each quotes as quote}
+  <div>
+    <h2>{quote.author}</h2>
+    <p>{quote.quote}</p>
+  </div>
+{/each}
+</div>
+<h1>Users</h1>
+<div>
+{#each users as user}
+  <div>
+    <h2>{user.firstName}</h2>
+    <p>{user.email}</p>
+  </div>
+{/each}
+</div>
+
+
+
+
+<style>
+    h1 {
+        justify-content: center;
+        font-family: monospace;
+        color: #333;
+        text-decoration: none;
+    }
+    
+    div {
+        justify-content: center;
+        font-family: monospace;
+        color: #333;
+        text-decoration: none;
+        flex-direction: column;
+        
+    }
+  
+    h2 {
+        justify-content: left;
+        font-family: monospace;
+        color: #333;
+        text-decoration: none;
+    }
+    p {
+        justify-content: center;
+        font-family: monospace;
+        color: #333;
+        text-decoration: none;
+    }
+</style>

9b7b60f7aafdd8dfd90312b26789cf5db226da56
abhishekeyespace <abhishek@eyespacelenses.com>
2022-12-21 15:42:21 +1030

    fetching quotesinside a route

--- src/routes/quotes/+page.js
+++ src/routes/quotes/+page.js
@@ -1,5 +1,10 @@
-export const load = async () => {
-  return {
-    quotes : "Hello World!"
-  }
+/**
+ *  fetch fuction behaves identically to the native fetch web API
+ **/
+
+export const load = async ({ fetch }) => {
+  const res = await fetch('https://dummyjson.com/quotes?limit=10')
+  const quotesData = await res.json()
+  const quotes = quotesData.quotes
+  return { quotes }
 }
